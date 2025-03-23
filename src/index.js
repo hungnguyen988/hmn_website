d51f1490df241045a92a9693f51572ecfffab7e8
hungnguyen988 <hungnguuen988@gmail.com>
2025-03-23 15:32:45 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/hmn_website">
     {/*// <HashRouter>*/}
         <React.StrictMode>
             <App />
         </React.StrictMode>
     {/*// </HashRouter>*/}
-    </BrowserRouter>
+     </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
